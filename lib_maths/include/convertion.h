2fb95ce7c0dbb0e4c159733baa6f5017a551bb12
Hugo PREVOST <DipStax.hugo@gmail.com>
2019-12-15 00:58:46 +0100

    Adding header
    [ADD]: lib_maths header:
    
    include of convetion.h
    
    [ADD]: convertion header:
    
    convetion

---
+++ lib_maths/include/convertion.h
@@ -0,0 +1,16 @@
+/*
+** DipStax, 10 December 2019
+** lib_maths
+** File description:
+** header for every convertion function
+*/
+
+#ifndef CONVERTION_H_
+#define CONVERTION_H_
+
+#include "constant.h"
+
+float rad_to_deg(float rad);
+float deg_to_rad(float deg);
+
+#endif
--- lib_maths/include/lib_maths.h
+++ lib_maths/include/lib_maths.h
@@ -11,5 +11,6 @@
 #include "constant.h"
 #include "trigonometry.h"
 #include "algebra.h"
+#include "convertion.h"
 
 #endif
